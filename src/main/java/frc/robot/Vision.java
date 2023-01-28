e78e910af03bf07bf6128c9a8db148c16419e2b8
ZaddyDwayne <118773412+ZaddyDwayne@users.noreply.github.com>
2023-01-28 16:50:27 -0500

    Fix warning

--- src/main/java/frc/robot/Vision.java
+++ src/main/java/frc/robot/Vision.java
@@ -14,7 +14,6 @@ import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.CvSink;
 import edu.wpi.first.cscore.CvSource;
 import edu.wpi.first.cscore.UsbCamera;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Vision {
     AprilTagDetector m_detector;
